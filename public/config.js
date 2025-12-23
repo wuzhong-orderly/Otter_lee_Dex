7e67cf76fbf9c1937be5e1327f1a66f530b3df7c
Wuzhong <wuzhong@orderly.network>
2025-12-23 22:20:40 +0800

    增加config file description

--- public/config.js
+++ public/config.js
@@ -1,41 +1,41 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "otter_lee",
-  "VITE_ORDERLY_BROKER_NAME": "Otter liquid",
+  "VITE_ORDERLY_BROKER_ID": "otter_lee", // 要改成自己的
+  "VITE_ORDERLY_BROKER_NAME": "Otter liquid", // 要改成自己的
   "VITE_BROKER_EOA_ADDRESS": "0x6c110236087c6e8449ba2d88a095b6d0788c5a02",
-  "VITE_DISABLE_MAINNET": "false",
-  "VITE_DISABLE_TESTNET": "false",
-  "VITE_ORDERLY_MAINNET_CHAINS": "42161,10,8453,1,56,900900900",
-  "VITE_ORDERLY_TESTNET_CHAINS": "421614,97,901901901,11124",
-  "VITE_DEFAULT_CHAIN": "",
-  "VITE_PRIVY_APP_ID": "cmcbqfeef004wl40m3jlc1tdz",
+  "VITE_DISABLE_MAINNET": "false", // 设置true可以禁用主网
+  "VITE_DISABLE_TESTNET": "false", // 设置true可以禁用测试网
+  "VITE_ORDERLY_MAINNET_CHAINS": "42161,10,8453,1,56,900900900", // 要支持的主网链id列表，用逗号分隔
+  "VITE_ORDERLY_TESTNET_CHAINS": "421614,97,901901901,11124", // 要支持的测试网链id列表，用逗号分隔
+  "VITE_DEFAULT_CHAIN": "", // 默认链id，留空则为列表中的第一个
+  "VITE_PRIVY_APP_ID": "cmcbqfeef004wl40m3jlc1tdz",  // 要改成自己的
   "VITE_PRIVY_LOGIN_METHODS": "email,passkey,twitter,google",
   "VITE_PRIVY_TERMS_OF_USE": "",
-  "VITE_ENABLE_ABSTRACT_WALLET": "true",
-  "VITE_DISABLE_EVM_WALLETS": "false",
-  "VITE_DISABLE_SOLANA_WALLETS": "false",
-  "VITE_WALLETCONNECT_PROJECT_ID": "34b79edc0603a7f1c0aef124674c14f2",
-  "VITE_APP_NAME": "Otter liquid",
-  "VITE_APP_DESCRIPTION": "Orderly Trading Application",
+  "VITE_ENABLE_ABSTRACT_WALLET": "true", // 设置true启用Abstract钱包
+  "VITE_DISABLE_EVM_WALLETS": "false", // 设置true禁用所有EVM钱包
+  "VITE_DISABLE_SOLANA_WALLETS": "false", // 设置true禁用所有Solana钱包
+  "VITE_WALLETCONNECT_PROJECT_ID": "34b79edc0603a7f1c0aef124674c14f2", // 要改成自己的
+  "VITE_APP_NAME": "Otter liquid", // 要改成自己的
+  "VITE_APP_DESCRIPTION": "Orderly Trading Application", // 要改成自己的
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Rewards,Markets,Leaderboard,Vaults",
-  "VITE_CUSTOM_MENUS": "",
-  "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Rewards,Markets,Leaderboard,Vaults", // 要启用的菜单列表，用逗号分隔
+  "VITE_CUSTOM_MENUS": "", // 自定义菜单, 详情见Orderly One
+  "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false", // 设置true启用服务免责声明弹窗
   "VITE_ENABLE_CAMPAIGNS": "true",
-  "VITE_TELEGRAM_URL": "",
-  "VITE_DISCORD_URL": "",
-  "VITE_TWITTER_URL": "",
-  "VITE_SEO_SITE_NAME": "",
+  "VITE_TELEGRAM_URL": "", // Telegram链接
+  "VITE_DISCORD_URL": "", // Discord链接
+  "VITE_TWITTER_URL": "", // Twitter链接  
+  "VITE_SEO_SITE_NAME": "", // SEO站点名称
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/wz-test-2220/",
+  "VITE_SEO_SITE_URL": "https://dex.orderly.network/wz-test-2220/", // SEO站点链接, 要改成自己的 
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
   "VITE_SEO_THEME_COLOR": "#000000",
-  "VITE_SEO_KEYWORDS": "",
-  "VITE_AVAILABLE_LANGUAGES": "en,zh,ja,es,ko,vi,de,fr,ru,id,tr,it,pt,uk,pl,nl",
-  "VITE_USE_CUSTOM_PNL_POSTERS": "true",
-  "VITE_CUSTOM_PNL_POSTER_COUNT": "1",
+  "VITE_SEO_KEYWORDS": "", // SEO关键词
+  "VITE_AVAILABLE_LANGUAGES": "en,zh,ja,es,ko,vi,de,fr,ru,id,tr,it,pt,uk,pl,nl", // 可用语言列表，用逗号分隔
+  "VITE_USE_CUSTOM_PNL_POSTERS": "true", // 设置true启用自定义social media海报
+  "VITE_CUSTOM_PNL_POSTER_COUNT": "1",  // 自定义social media海报数量
   "VITE_TRADING_VIEW_COLOR_CONFIG": "{\"upColor\":\"#0d00c7\",\"downColor\":\"#f7dc55\",\"pnlUpColor\":\"#c7b600\",\"pnlDownColor\":\"#ff9ea6\",\"chartBG\":\"#ffffff\"}",
   "VITE_ANALYTICS_SCRIPT": ""
 };
